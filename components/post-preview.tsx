9fb5db9b1e73c52c8b958a48bcd43965415ec641
Aria(김인숙) <kis@gabia.com>
2023-09-06 13:54:16 +0900

    feat: 블로그 미리보기 이미지 없는 경우 처리

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -24,7 +24,9 @@ const PostPreview = ({
   return (
     <div>
       <div className="mb-5">
-        <CoverImage slug={slug} title={title} src={coverImage} />
+          {coverImage ? (
+              <CoverImage title={title} src={coverImage} slug={slug} />
+          ):null}
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
         <Link
